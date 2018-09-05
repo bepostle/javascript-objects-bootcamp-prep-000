346a0e41c2eda926b67fffdc0a33881daa6ef307
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 01:14:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -46,5 +46,6 @@ function updatePlaylist (playlist, artist, song) {
 }
 
 function removeFromPlaylist (playlist, artist) {
-  return delete playlist.artist;
+  delete playlist.artist;
+  return playlist;
 }
